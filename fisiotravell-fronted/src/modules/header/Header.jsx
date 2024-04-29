382a916632f61fe26451db2f54761a40083d820c
mjdev93 <mjurado1993@gmail.com>
2024-04-29 22:59:03 +0200

    header terminado css

--- fisiotravell-fronted/src/modules/header/Header.jsx
+++ fisiotravell-fronted/src/modules/header/Header.jsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
 import './header.css'
-import logo from '../../assets/images/logofisiotravell.webp'
+import logo from '/images/logofisiotravell-blanco.webp'
 function Header() {
 
   return (
     <>
-     <header>
-        <nav>
-            <section className='logo'>
-                <img src={logo} alt='Logo de Fisio Travell'/>
-            </section>
-            <section className='menu'>
-                <button className='servicios'>SERVICIOS</button>
-                <button className='metodologia'>METODOLOGÍA</button>
-                <button className='equipo'>EL EQUIPO</button>
-                <button className='articulos'>ARTÍCULOS</button>
-                <button className='videos'>VÍDEOS</button>
-                <button className='cita remark'>PIDE CITA</button>
-                <button className='empresas remark'>EMPRESAS</button>
-                <button className='login remark'>PERFIL</button>
-            </section>
-            </nav>
-        <section></section>
-     </header>
+      <header className='main-header'>
+        <nav className='nav'>
+          <section className='logo'>
+            <img src={logo} alt='Logo de Fisio Travell' />
+          </section>
+          <section className='menu'>
+            <button className='servicios tab'>SERVICIOS</button>
+            <button className='metodologia tab'>METODOLOGÍA</button>
+            <button className='equipo tab'>EL EQUIPO</button>
+            <button className='articulos tab'>ARTÍCULOS</button>
+            <button className='videos tab'>VÍDEOS</button>
+            <button className='cita remark'>PIDE CITA</button>
+            <button className='empresas remark'>EMPRESAS</button>
+            <button className='login remark'>PERFIL</button>
+          </section>
+        </nav>
+        <section className='text'>
+          <h2>Apuesta en salud y habrás ganado</h2>
+          <button className='cita remark hero'>PIDE CITA</button>
+        </section>
+      </header>
     </>
   )
 }
