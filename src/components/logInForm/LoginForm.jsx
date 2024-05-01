bb652423b38c3012feb1df84103d6b2f0601b0a2
JLLD47 <oormzoo@gmail.com>
2024-05-01 23:12:40 +0100

    log in view done and some changes in css of sign up and log in

--- src/components/logInForm/LoginForm.jsx
+++ src/components/logInForm/LoginForm.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./loginForm.css";
+import logo from "../../../public/images/icono-hojas.webp"
 
 export const LoginForm = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const emailRef = useRef();
 
-export default LoginForm
+  const [email, setEmail] = useState("");
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [pass, setPass] = useState("");
+  const [passFocus, setPassFocus] = useState(false);
+
+  return (
+    <div className="logContainer">
+      <div className="logBox">
+      <img src={logo} className="successIcon" alt="Logo de Fisio Travell" />
+
+      <h1>Accede</h1>
+      <form className="logForm">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          ref={emailRef}
+          autoComplete="on"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          onFocus={() => setEmailFocus(true)}
+          onBlur={() => setEmailFocus(false)}
+        />
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPass(e.target.value)}
+          required
+          onFocus={() => setPassFocus(true)}
+          onBlur={() => setPassFocus(false)}
+        />
+        <button className="logButton">Aceptar</button>
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
