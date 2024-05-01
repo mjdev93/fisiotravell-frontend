b382ab204c89f8f54e0934470fd215873de15347
JLLD47 <oormzoo@gmail.com>
2024-05-01 22:06:48 +0100

    added success screen when the user register (jsx and css)

--- src/components/signUp/SignUpForm.jsx
+++ src/components/signUp/SignUpForm.jsx
@@ -7,12 +7,12 @@ import {
   faCircleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-
+import logo from "../../../public/images/icono-hojas.webp"
 import React from "react";
 
 const USER_REGEX = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]{3,30}$/;
 const LASTNAME_REGEX = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]{3,30}$/;
-const PHONE_REGEX = /^\d{9}$/; 
+const PHONE_REGEX = /^\d{9}$/;
 
 const EMAIL_REGEX = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 const PASS_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -79,190 +79,219 @@ const SignUpForm = () => {
     setValidMatch(match);
   }, [pass, matchPass]);
 
-  useEffect(() => setErrMsg("")), [name, email, pass, matchPass];
+  useEffect(() => setErrMsg("")), [user, email, pass, matchPass];
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const secureEmail = EMAIL_REGEX.test(email);
+    const securePass = PASS_REGEX.test(pass);
+    const secureUser = USER_REGEX.test(user);
+
+    if (!secureEmail || !securePass || !secureUser) {
+      setErrMsg("Not valid entry");
+      return;
+    }
+    console.log(user, pass);
+    setSuccess(true);
+  };
 
   return (
-    <div className="loginContainer">
-      <div className="loginBox">
-        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>
-          {errMsg}
-        </p>
-        <h1 className="titleLog">Registro</h1>
-        <form>
-          <label htmlFor="user" className="asterisk">
-            Nombre:
-            {validUser && <FontAwesomeIcon icon={faCheck} className="valid" />}
-            {!validUser && user && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setUser(e.target.value)}
-            required
-            onFocus={() => setUserFocus(true)}
-            onBlur={() => setUserFocus(false)}
-          />
-          <p
-            className={
-              !validUser && userFocus && user ? "instructions" : "hide"
-            }
-          >
-            <FontAwesomeIcon icon={faCircleExclamation} />
-            Debe tener mínimo tres letras. <br />
+    <>
+      {success ? (
+        <div className="successContainer">
+        <div className="successRegister">
+          <h1 className="successTitle">Registro exitoso</h1>
+          <img src={logo} className="successIcon" alt='Logo de Fisio Travell' />
+          <button className="successButton"><Link to="/login">Accede</Link></button>
+        </div>
+        </div>
+      ) : (
+        <div className="loginContainer">
+          <p ref={errRef} className={errMsg ? "errmsg" : "hide"}>
+            {errMsg}
           </p>
-        </form>
+          <div className="loginBox">
+            <h1 className="titleLog">Registro</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="user" className="asterisk">
+                Nombre:
+                {validUser && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validUser && user && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setUser(e.target.value)}
+                required
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              />
+              <p
+                className={
+                  !validUser && userFocus && user ? "instructions" : "hide"
+                }
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                Debe tener mínimo tres letras. <br />
+              </p>
 
-        <form>
-          <label htmlFor="lastName">
-            Apellidos:
-            {validlastName && (
-              <FontAwesomeIcon icon={faCheck} className="valid" />
-            )}
-            {!validlastName && lastName && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            onChange={(e) => setLastName(e.target.value)}
-            onFocus={() => setLastNameFocus(true)}
-            onBlur={() => setLastNameFocus(false)}
-          />
-          <p
-            className={
-              !validlastName && lastNameFocus && lastName
-                ? "instructions"
-                : "hide"
-            }
-          >
-            <FontAwesomeIcon icon={faCircleExclamation} />
-            Debe tener mínimo tres letras. <br />
-          </p>
-        </form>
-        <form>
-          <label htmlFor="phone">
-            Teléfono:
-            {validPhone && <FontAwesomeIcon icon={faCheck} className="valid" />}
-            {!validPhone && phone && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="text"
-            id="phone"
-            onChange={(e) => setPhone(e.target.value)}
-            onFocus={() => setPhoneFocus(true)}
-            onBlur={() => setPhoneFocus(false)}
-          />
-          <p
-            className={
-              !validPhone && phoneFocus && phone
-                ? "instructions"
-                : "hide"
-            }
-          >
-            <FontAwesomeIcon icon={faCircleExclamation} />
-            Debe tener 9 dígitos.
-          </p>
-        </form>
-        <form>
-          <label htmlFor="email" className="asterisk">
-            Email:
-            {validEmail && <FontAwesomeIcon icon={faCheck} className="valid" />}
-            {!validEmail && email && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="email"
-            id="email"
-            ref={emailRef}
-            autoComplete="on"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            onFocus={() => setEmailFocus(true)}
-            onBlur={() => setEmailFocus(false)}
-            aria-invalid={validEmail ? "false" : "true"}
-            aria-describedby="emailnote"
-          />
-        </form>
-        <form>
-          <label htmlFor="password" className="asterisk">
-            Contraseña:
-            {validPass && <FontAwesomeIcon icon={faCheck} className="valid" />}
-            {!validPass && pass && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPass(e.target.value)}
-            required
-            onFocus={() => setPassFocus(true)}
-            onBlur={() => setPassFocus(false)}
-          />
-          <p
-            className={
-              !validPass && passFocus && pass ? "instructions" : "hide"
-            }
-          >
-            <FontAwesomeIcon icon={faCircleExclamation} />
-            Debe tener entre 8 y 24 caracteres. <br />
-            Debe contener al menos minúscula, una mayúscula, un número y un
-            símbolo. <br />
-            Símbolos permitidos: !@#$% <br />
-          </p>
-        </form>
-        <form>
-          <label htmlFor="password">
-            Confirme la contraseña:
-            {validMatch && matchPass && (
-              <FontAwesomeIcon icon={faCheck} className="valid" />
-            )}
-            {!validMatch && matchPass && (
-              <FontAwesomeIcon icon={faXmark} className="invalid" />
-            )}
-          </label>
-          <input
-            type="password"
-            id="confirmPass"
-            onChange={(e) => setMatchPass(e.target.value)}
-            value={matchPass}
-            required
-            onFocus={() => setMatchFocus(true)}
-            onBlur={() => setMatchFocus(false)}
-          />
-          <p
-            className={
-              !validMatch && matchFocus && matchPass ? "instructions" : "hide"
-            }
-          >
-            <FontAwesomeIcon icon={faCircleExclamation} />
-            Las contraseñas no coinciden.
-          </p>
-        </form>
-        <span className="buttonContainer">
-          <button
-            className="logButton"
-            disabled={!validEmail || !validPass || !validMatch || !validUser ? true : false}
-          >
-            Registrarse
-          </button>
-        </span>
+              <label htmlFor="lastName">
+                Apellidos:
+                {validlastName && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validlastName && lastName && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                onChange={(e) => setLastName(e.target.value)}
+                onFocus={() => setLastNameFocus(true)}
+                onBlur={() => setLastNameFocus(false)}
+              />
+              <p
+                className={
+                  !validlastName && lastNameFocus && lastName
+                    ? "instructions"
+                    : "hide"
+                }
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                Debe tener mínimo tres letras. <br />
+              </p>
+              <label htmlFor="phone">
+                Teléfono:
+                {validPhone && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validPhone && phone && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="text"
+                id="phone"
+                onChange={(e) => setPhone(e.target.value)}
+                onFocus={() => setPhoneFocus(true)}
+                onBlur={() => setPhoneFocus(false)}
+              />
+              <p
+                className={
+                  !validPhone && phoneFocus && phone ? "instructions" : "hide"
+                }
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                Debe tener 9 dígitos.
+              </p>
+              <label htmlFor="email" className="asterisk">
+                Email:
+                {validEmail && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validEmail && email && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="email"
+                id="email"
+                ref={emailRef}
+                autoComplete="on"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                onFocus={() => setEmailFocus(true)}
+                onBlur={() => setEmailFocus(false)}
+                aria-invalid={validEmail ? "false" : "true"}
+                aria-describedby="emailnote"
+              />
+              <label htmlFor="password" className="asterisk">
+                Contraseña:
+                {validPass && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validPass && pass && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPass(e.target.value)}
+                required
+                onFocus={() => setPassFocus(true)}
+                onBlur={() => setPassFocus(false)}
+              />
+              <p
+                className={
+                  !validPass && passFocus && pass ? "instructions" : "hide"
+                }
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                Debe tener entre 8 y 24 caracteres. <br />
+                Debe contener al menos minúscula, una mayúscula, un número y un
+                símbolo. <br />
+                Símbolos permitidos: !@#$% <br />
+              </p>
+              <label htmlFor="password">
+                Confirme la contraseña:
+                {validMatch && matchPass && (
+                  <FontAwesomeIcon icon={faCheck} className="valid" />
+                )}
+                {!validMatch && matchPass && (
+                  <FontAwesomeIcon icon={faXmark} className="invalid" />
+                )}
+              </label>
+              <input
+                type="password"
+                id="confirmPass"
+                onChange={(e) => setMatchPass(e.target.value)}
+                value={matchPass}
+                required
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+              <p
+                className={
+                  !validMatch && matchFocus && matchPass
+                    ? "instructions"
+                    : "hide"
+                }
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                Las contraseñas no coinciden.
+              </p>
+              <span className="buttonContainer">
+                <button
+                  className="logButton"
+                  disabled={
+                    !validEmail || !validPass || !validMatch || !validUser
+                      ? true
+                      : false
+                  }
+                >
+                  Registrarse
+                </button>
+              </span>
+            </form>
 
-        <p>¿Ya estas registrado?</p>
-        <span className="access">
-          <Link to="/Login">Acceder</Link>
-        </span>
-        <span>
-          <p className="required">*Campos requeridos.</p>
-        </span>
-      </div>
-    </div>
+            <p>¿Ya estas registrado?</p>
+            <span className="access">
+              <Link to="/Login">Acceder</Link>
+            </span>
+            <span>
+              <p className="required">*Campos requeridos.</p>
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
