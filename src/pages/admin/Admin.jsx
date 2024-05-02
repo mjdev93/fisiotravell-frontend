1375d699d26142a9ebe48a54b12c012d7800f3c2
Blaynei <beatriz.rrm@gmail.com>
2024-05-02 22:42:37 +0200

    merging

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import SearchBar from '../../components/searchBar/SearchBar'
-
-
-
-
 import React from "react";
-import CarouselVideo from "../../components/carousel/CarouselVideo";
-import AdminProfile from "../../components/adminProfile/AdminProfile";
+
 
 
 
