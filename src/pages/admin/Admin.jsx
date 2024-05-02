be0f24e58346344a60e6322491a4ceb3d4ee829d
Blaynei <beatriz.rrm@gmail.com>
2024-05-02 22:38:41 +0200

    "merging"
    "# Please enter the commit message for your changes. Lines starting

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -4,29 +4,26 @@ import SearchBar from '../../components/searchBar/SearchBar'
 
 
 
-=======
+
 import React from "react";
 import CarouselVideo from "../../components/carousel/CarouselVideo";
 import AdminProfile from "../../components/adminProfile/AdminProfile";
-import AdminHeader from "../../components/adminHeader/AdminHeader"
->>>>>>> main
+
+
 
 const Admin = () => {
   return (
     <>
-<<<<<<< HEAD
+
     
     <SearchBar/>
     </>
   )
 }
-=======
-      <AdminHeader />
-      <AdminProfile />
-      <CarouselVideo />
-    </>
-  );
-};
->>>>>>> main
+
+
+    
+
+
 
 export default Admin;
