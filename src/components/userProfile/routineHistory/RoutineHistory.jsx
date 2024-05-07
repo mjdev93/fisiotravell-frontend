1fb3c83443aa62b5a19de8ff2c53008e34930f22
JLLD47 <oormzoo@gmail.com>
2024-05-07 19:10:53 +0100

    dates working and nice format

--- src/components/userProfile/routineHistory/RoutineHistory.jsx
+++ src/components/userProfile/routineHistory/RoutineHistory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getMyRoutines } from "../../../services/history.service";
-
+import moment from "moment/moment";
 
 const RoutineHistory = () => {
 
@@ -8,13 +8,14 @@ const RoutineHistory = () => {
 
   useEffect(()=>{
     const fetchMyRoutines = async () =>{
-      const data = await getMyRoutines()
+      const {data} = await getMyRoutines()
         setMyRoutines(data)
     }
     fetchMyRoutines()
   }, [])
   
 const displayRoutines = () => {
+  console.log(myRoutines)
   const formatRoutines = myRoutines.map((routine, index) => {
   return (
     <div key={index}>
