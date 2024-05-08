49eac9c6013dc8698df04fc18cb443aae0a043cf
JLLD47 <oormzoo@gmail.com>
2024-05-08 22:14:38 +0100

    Profile almost done

--- src/components/userProfile/routineHistory/RoutineHistory.jsx
+++ src/components/userProfile/routineHistory/RoutineHistory.jsx
@@ -4,50 +4,52 @@ import moment from "moment/moment";
 import "./routineHistory.css";
 
 const RoutineHistory = () => {
-
   const [myRoutines, setMyRoutines] = useState([]);
 
-  useEffect(()=>{
-    const fetchMyRoutines = async () =>{
-      const {data} = await getMyRoutines()
-        setMyRoutines(data)
-    }
-    fetchMyRoutines()
-  }, [])
-  
-const displayRoutines = () => {
-  console.log(myRoutines)
-  const formatRoutines = myRoutines.map((routine, index) => {
-    const extractedDate = routine.date
-    const niceDate= moment(extractedDate).format('D/MM/YYYY')
+  useEffect(() => {
+    const fetchMyRoutines = async () => {
+      const { data } = await getMyRoutines();
+      setMyRoutines(data);
+    };
+    fetchMyRoutines();
+  }, []);
+
+  const displayRoutines = () => {
+      const formatRoutines = myRoutines.map((routine, index) => {
+      const extractedDate = routine.date;
+      const niceDate = moment(extractedDate).format("D/MM/YYYY");
+
+      return (
+        <>
+          <details key={index} name="historyRoutine">
+            <summary className="fechaProfileUser">{niceDate}</summary>
+            <div>
+              <p>{displayExercises(routine.exercises)}</p>
+            </div>
+          </details>
+          <hr key={`hr-${index}`} />
+        </>
+      );
+    });
+    return formatRoutines;
+  };
+
+  const displayExercises = (exercises) => {
+    console.log(exercises)
+    return exercises.map((exercise, index) => (
+      <div key={`exercise-${index}`}>
+        <p>{exercise.title}</p>
+        <p>{exercise.videoUrl}</p>
+        <p>{exercise.description}</p>
+      </div>
+    ));
+  };
+
   return (
     <>
-    
-<details key={index} name="historyRoutine">
-        <summary className="fechaProfileUser">{routine.date}</summary>
-        <div>
-            <p>Contenido</p>
-        </div>
-      </details>
-      <hr/>
-      </>
-
-
-
-
-  )})
-  return formatRoutines
-}
-
-
-    
-  return (
-  <>
-  <div>
-      {displayRoutines()}
-  </div>
-  </>
-  )
+      <div>{displayRoutines()}</div>
+    </>
+  );
 };
 
 export default RoutineHistory;
