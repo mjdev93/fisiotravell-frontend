a2ae8bf2ba54b6573ea3a0d4a260925da49c38a8
JLLD47 <oormzoo@gmail.com>
2024-05-09 08:31:03 +0100

    fixed search function on searchBar

--- src/components/adminPage/searchBar/SearchBar.jsx
+++ src/components/adminPage/searchBar/SearchBar.jsx
@@ -1,8 +1,9 @@
 import "./searchBar.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Table from "../table/Table";
 import { getAllUsers } from "../../../services/admin.service"
 import CreateUser from "../createUser/CreateUser";
+import { InfoContext } from "../../../context/infoContext";
 
 
 
@@ -10,8 +11,7 @@ function SearchBar() {
   const [query, setQuery] = useState("");
   const [userData, setUserData] = useState([])
   const [showCreateUser, setShowCreateUser] = useState(false)
-  // const [adminName, setAdminName] = useState("");
-
+  const {info} = useContext(InfoContext)
   const keys = ["name", "lastname", "email", "phone"];
 
   useEffect(() => {
@@ -30,22 +30,10 @@ function SearchBar() {
   }, [userData]);
 
 
-  // const fetchAdminInfo = async () => {
-  //   try {
-  //     const users = await getAllUsers();
-  //     const admin = users[0]; // Suponiendo que el primer usuario es el administrador
-  //     setAdminName(admin.name); // Establecer el nombre del administrador en el estado
-  //   } catch (error) {
-  //     console.error("Error al obtener la información del administrador", error);
-  //   }
-  // };
-
-  // fetchAdminInfo();
-
-
   const search = (data) => {
+    if (!data || data.length === 0) return [];
     return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query))
+      keys.some((key) => (item[key] || "").toLowerCase().includes(query))
     );
   };
 
